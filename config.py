50c400253945952c1adf77230fb12523a0c24a29
杜鹏程 <dupengcheng888@outlook.com>
2019-08-03 11:31:56 +0800

    多直播间抓取

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ ROOMS = {
     "xj": 74751,
     "yz": 71415,
     "lx": 52319,
+    "6079602": 6079602,
+    "by": 252802,
 }
 
 # 每几条数据写入数据库一次
@@ -17,5 +19,5 @@ SAVE_CONFIG = {
     52319: 3,
 }
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://user:pw@localhost/db")
-DB_NAME = "db"
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://ddyy:123@localhost/danmu")
+DB_NAME = "danmu"
